6874056656d3fe46cdae25d34cf4a8034f1c8ab0
miladmxm <miladmxm12@gmail.com>
2025-12-06 23:42:58 +0330

    add: side bar component in features

--- src/components/dashboard/app-sidebar.tsx
+++ src/components/dashboard/app-sidebar.tsx
@@ -169,7 +169,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <Link href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+                <span className="text-base font-semibold"></span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
---
+++ src/features/auth/admin/components/sidebar.tsx
@@ -0,0 +1,25 @@
+import type { FC, PropsWithChildren } from "react";
+
+import { AppSidebar } from "@/components/dashboard/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/dashboard/ui/sidebar";
+
+const SidebarWrapper: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>{children}</SidebarInset>
+    </SidebarProvider>
+  );
+};
+
+export default SidebarWrapper;
