829bea07bd2687a24ff3d2dd3ce4b56f6ac626b7
miladmxm <miladmxm12@gmail.com>
2026-04-30 01:47:24 +0330

    fix: search params for infinity scroll

--- src/app/admin/(dashboard)/blog/[id]/page.tsx
+++ src/app/admin/(dashboard)/blog/[id]/page.tsx
@@ -1,8 +1,11 @@
 import EditArticle from "@/features/article/containers/editArticle";
 
-const EditArticlePage = async ({ params }: PageProps<"/admin/blog/[id]">) => {
+const EditArticlePage = async ({
+  params,
+  searchParams,
+}: PageProps<"/admin/blog/[id]">) => {
   const { id } = await params;
-  return <EditArticle id={id} />;
+  return <EditArticle id={id} searchParams={await searchParams} />;
 };
 
 export default EditArticlePage;
--- src/features/article/containers/editArticle.tsx
+++ src/features/article/containers/editArticle.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
 
+import type { SearchParams } from "@/types/type";
+
 import { dalVerifySuccess } from "@/dal/helpers";
 import MediaContextProvider from "@/features/media/context";
 import { getMediasByType } from "@/features/media/dal/queries";
@@ -7,11 +9,17 @@ import { getMediasByType } from "@/features/media/dal/queries";
 import EditArticleForm from "../components/editArticleForm";
 import { getArticle, getCategories } from "../dal/query";
 
-const EditArticle = async ({ id }: { id: string }) => {
+const EditArticle = async ({
+  id,
+  searchParams,
+}: {
+  id: string;
+  searchParams?: SearchParams;
+}) => {
   const article = dalVerifySuccess(await getArticle(id));
   if (!article) redirect("/admin");
-  const images = getMediasByType(["image"]);
-  const audios = getMediasByType(["audio"]);
+  const images = getMediasByType(["image"], searchParams);
+  const audios = getMediasByType(["audio"], searchParams);
   const categories = getCategories();
 
   return (
