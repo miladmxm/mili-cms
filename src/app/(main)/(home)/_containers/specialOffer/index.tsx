32c43aa1ee9e60d0001b96af7d15763a3fbe655a
miladmxm <miladmxm12@gmail.com>
2026-05-12 16:50:12 +0330

    chore: add section curve component

--- src/app/(main)/(home)/_containers/specialOffer/index.tsx
+++ src/app/(main)/(home)/_containers/specialOffer/index.tsx
@@ -1,4 +1,5 @@
 import H3 from "@/components/ui/h2";
+import SectionCurve from "@/components/ui/sectionCurve";
 import { getDiscountedProducts } from "@/features/product/dal/query";
 
 import OfferCarusel from "./offerCarusel";
@@ -7,7 +8,7 @@ const SpecialOffer = async () => {
   const products = await getDiscountedProducts();
   return (
     <section className="bg-gradient-to-b isolate from-thready-200 relative to-primary-25 pt-28 md:pt-40">
-      <div className="bg-primary-25 -z-10 h-28 md:h-40 w-full absolute top-0 -translate-y-1/2 rounded-[100%]" />
+      <SectionCurve />
       <div className="container flex flex-col gap-8">
         <H3>پیشنهاد ویژه</H3>
         <OfferCarusel products={products} />
---
+++ src/components/ui/sectionCurve.tsx
@@ -0,0 +1,41 @@
+import type { VariantProps } from "class-variance-authority";
+import type { ComponentProps } from "react";
+
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const sectionCurveVariants = cva(
+  "-z-10 h-28 md:h-40 w-full absolute rounded-[100%]",
+  {
+    variants: {
+      color: {
+        default: "bg-primary-25",
+      },
+      position: {
+        top: "top-0 -translate-y-1/2",
+        bottom: "bottom-0 translate-y-1/2",
+      },
+    },
+    defaultVariants: {
+      color: "default",
+      position: "top",
+    },
+  },
+);
+
+const SectionCurve = ({
+  color,
+  position,
+  className,
+  ...props
+}: ComponentProps<"div"> & VariantProps<typeof sectionCurveVariants>) => {
+  return (
+    <div
+      className={cn(sectionCurveVariants({ color, position, className }))}
+      {...props}
+    />
+  );
+};
+
+export default SectionCurve;
