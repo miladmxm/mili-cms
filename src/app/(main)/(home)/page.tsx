67fe62a09a49200d0eef375bad7b1710df65f3c7
miladmxm <miladmxm12@gmail.com>
2026-05-14 19:06:19 +0330

    feat (installment section): complite and responsive

--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -3,6 +3,7 @@ import Categories from "./_containers/categories";
 import CropSVG from "./_containers/cropSVG";
 import GoodPriceProducts from "./_containers/goodPrice";
 import Hero from "./_containers/hero";
+import InstallmentTerms from "./_containers/installmentTerms";
 import MoreProducts from "./_containers/moreProducts";
 import ProductDetails from "./_containers/productDetails";
 import SpecialOffer from "./_containers/specialOffer";
@@ -17,6 +18,7 @@ const Home = async () => {
       <AugmentedReality />
       <MoreProducts />
       <ProductDetails />
+      <InstallmentTerms />
       <CropSVG />
     </main>
   );
