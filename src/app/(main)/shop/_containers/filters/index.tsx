b4f1925f507348994b413cc11038281d879c8d2c
miladmxm <miladmxm12@gmail.com>
2026-05-31 17:56:17 +0330

    chore: mobile size filter box

--- src/app/(main)/shop/_containers/filters/index.tsx
+++ src/app/(main)/shop/_containers/filters/index.tsx
@@ -5,14 +5,18 @@ import { useEffect } from "react";
 
 import type { Option } from "@/services/product/type";
 
+import Close from "@/assets/icons/close.svg";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { cn } from "@/lib/utils";
+
 import FilterByOptions from "./filterByOptions";
 import FilterByPrice from "./filterByPrice";
 import FilterDiscounted from "./filterDiscounted";
-import { useFilterMenuStore } from "./store";
+import { setCloseFilter, useFilterMenuStore } from "./store";
 
 const ShopFilters = ({ options }: { options: Option[] }) => {
   const isOpen = useFilterMenuStore((state) => state.open);
-
+  const isMobile = useIsMobile();
   const [sidebarRef, animate] = useAnimate<HTMLDivElement>();
   useEffect(() => {
     if (isOpen) {
@@ -23,17 +27,36 @@ const ShopFilters = ({ options }: { options: Option[] }) => {
   }, [animate, isOpen]);
   return (
     <motion.aside
-      animate={{ width: isOpen ? "30%" : "0" }}
+      animate={{ width: isOpen ? (isMobile ? "100%" : "auto") : "0" }}
+      layout
       transition={{ delay: isOpen ? 0 : 0.2 }}
-      className="w-0 overflow-hidden"
+      className={cn(
+        "w-0 overflow-hidden max-md:fixed top-0 isolate max-md:z-50 max-md:h-full right-0",
+      )}
     >
-      <div
-        ref={sidebarRef}
-        className="bg-primary-25 *:opacity-0 rounded-7xl px-8 py-6 flex flex-col gap-6"
-      >
-        <FilterByPrice />
-        <FilterByOptions options={options} />
-        <FilterDiscounted />
+      {isOpen && isMobile && (
+        <button
+          type="button"
+          className="absolute inset-0 backdrop-blur-xs -z-10"
+          onClick={setCloseFilter}
+        />
+      )}
+      <div className="max-md:bg-white flex h-full w-full max-md:w-9/10 flex-col">
+        <button
+          className="size-4 md:hidden ms-auto my-4 mx-4"
+          type="button"
+          onClick={setCloseFilter}
+        >
+          <Close />
+        </button>
+        <div
+          ref={sidebarRef}
+          className="bg-primary-25 max-md:overflow-y-auto max-md:h-full *:opacity-0 rounded-7xl px-8 py-6 flex flex-col gap-6"
+        >
+          <FilterByPrice />
+          <FilterByOptions options={options} />
+          <FilterDiscounted />
+        </div>
       </div>
     </motion.aside>
   );
--- src/app/(main)/shop/_containers/productsWrapper.tsx
+++ src/app/(main)/shop/_containers/productsWrapper.tsx
@@ -5,7 +5,7 @@ const ProductsWrapper = ({
 }: PropsWithChildren<{ className?: string }>) => {
   return (
     <div className="container">
-      <div className="bg-white rounded-4xl md:rounded-7xl p-8 z-10 relative shadow-blur">
+      <div className="bg-white rounded-4xl md:rounded-7xl p-8 z-30 relative shadow-blur">
         {children}
       </div>
     </div>
