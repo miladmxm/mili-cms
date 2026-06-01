11989ad0698e9c9c0dddbc11d246d326306bb04d
miladmxm <miladmxm12@gmail.com>
2026-06-01 15:12:38 +0330

    fix: product wrapper z position

--- src/app/(main)/shop/_containers/productsWrapper.tsx
+++ src/app/(main)/shop/_containers/productsWrapper.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import type { PropsWithChildren } from "react";
+
+import { cn } from "@/lib/utils";
 
 import type { FilterParamsState } from "../_context";
 
 import FilterParamsContextProvider from "../_context";
+import { useFilterMenuStore } from "./filters/store";
 
 const ProductsWrapper = ({
   children,
@@ -11,9 +16,15 @@ const ProductsWrapper = ({
   className?: string;
   contextValue: FilterParamsState;
 }>) => {
+  const { open } = useFilterMenuStore();
   return (
     <div className="container">
-      <div className="bg-white rounded-4xl md:rounded-7xl p-8 z-30 relative shadow-blur">
+      <div
+        className={cn(
+          "bg-white rounded-4xl md:rounded-7xl p-8 z-10 relative shadow-blur",
+          { "max-md:z-40": open },
+        )}
+      >
         <FilterParamsContextProvider params={contextValue}>
           {children}
         </FilterParamsContextProvider>
