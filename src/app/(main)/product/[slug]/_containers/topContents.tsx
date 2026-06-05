c69bb03f163fefd46f0dda4bf18050aae7367923
miladmxm <miladmxm12@gmail.com>
2026-06-06 00:23:58 +0330

    chore: create star rate

--- src/app/(main)/product/[slug]/_containers/topContents.tsx
+++ src/app/(main)/product/[slug]/_containers/topContents.tsx
@@ -1,9 +1,13 @@
 import type { Product } from "@/services/product/type";
 
-const TopContents = ({ name }: Product) => {
+import RateStars from "@/components/ui/rateStars";
+
+const TopContents = ({ name, variables }: Product) => {
+  console.log(variables);
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-gray-500 md:text-xl font-bold">{name}</h1>
+      <RateStars rate={12} className="w-44" />
     </div>
   );
 };
---
+++ src/components/ui/rateStars.tsx
@@ -0,0 +1,30 @@
+import type { CSSProperties } from "react";
+
+import Image from "next/image";
+
+import stars from "@/assets/images/stars.png";
+import starsOutline from "@/assets/images/starsOutline.png";
+import { cn } from "@/lib/utils";
+
+const RateStars = ({
+  rate,
+  className,
+}: {
+  rate: number;
+  className?: string;
+}) => {
+  const validatedRate = rate > 100 ? 100 : rate < 0 ? 0 : rate;
+  return (
+    <div className={cn("relative", className)}>
+      <Image
+        style={{ "--rate-number": validatedRate } as CSSProperties}
+        src={stars}
+        alt="stars"
+        className="clip-path-inset-e-[var(--rate-number)]"
+      />
+      <Image src={starsOutline} className="inset-0 absolute" alt="stars" />
+    </div>
+  );
+};
+
+export default RateStars;
