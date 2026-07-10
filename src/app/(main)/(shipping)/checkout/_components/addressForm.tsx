5e75ade10160c3e4480414d84dde766855eeaae9
miladmxm <miladmxm12@gmail.com>
2026-07-10 22:37:27 +0330

    chore: create address form

--- src/app/(main)/(shipping)/checkout/_components/addressForm.tsx
+++ src/app/(main)/(shipping)/checkout/_components/addressForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { ComponentProps } from "react";
+import type { ComponentProps, PropsWithChildren } from "react";
 
 import { Controller } from "react-hook-form";
 
@@ -11,9 +11,39 @@ const NormalTextInput = ({
   ...props
 }: ComponentProps<"input"> & { label: string }) => {
   return (
-    <div className="flex">
-      <label htmlFor={props.id}>{label}</label>
-      <input type="text" {...props} />
+    <div className="flex items-center gap-2">
+      <label
+        className="text-lg font-bold w-1/3 text-nowrap min-w-max"
+        htmlFor={props.id}
+      >
+        {label}:
+      </label>
+      <input
+        className="flex-auto py-1.5 px-2.5 rounded-full min-w-0 bg-white border border-primary-500"
+        type="text"
+        {...props}
+      />
+    </div>
+  );
+};
+
+const NormalTextarea = ({
+  label,
+  ...props
+}: ComponentProps<"textarea"> & { label: string }) => {
+  return (
+    <div className="flex gap-2">
+      <label
+        className="text-lg font-bold w-1/3 text-wrap min-w-max"
+        htmlFor={props.id}
+      >
+        {label}:
+      </label>
+      <textarea
+        rows={5}
+        className="flex-auto py-2 px-3 rounded-3xl bg-white border border-primary-500 resize-none"
+        {...props}
+      />
     </div>
   );
 };
@@ -22,27 +52,125 @@ const ErrorMessage = ({ message }: { message?: string }) => {
   return <small className="ps-2 text-sm text-error">{message}</small>;
 };
 
-const AddressForm = () => {
-  const { control, isPending, onSubmit } = useAddAddress();
+const FieldGroup = ({ children }: PropsWithChildren) => {
   return (
-    <section className="border border-primary-500 bg-primary-25">
+    <div className="grid grid-cols-2 gap-4 auto-rows-auto @max-2xl:grid-cols-1">
+      {children}
+    </div>
+  );
+};
+
+const AddressForm = () => {
+  const { control, onSubmit } = useAddAddress();
+  return (
+    <section className="border border-primary-500 bg-primary-25 py-4 px-8 rounded-4xl @container">
       <form onSubmit={onSubmit}>
-        <Controller
-          control={control}
-          name="fullname"
-          render={({ field, fieldState }) => (
-            <div>
-              <NormalTextInput
-                {...field}
-                label="نام و نام خانوادگی"
-                placeholder="علیرضا حسینی"
-              />
-              {fieldState.invalid && (
-                <ErrorMessage message={fieldState.error?.message} />
-              )}
-            </div>
-          )}
-        />
+        <FieldGroup>
+          <Controller
+            control={control}
+            name="fullname"
+            render={({ field, fieldState }) => (
+              <div>
+                <NormalTextInput
+                  {...field}
+                  id={field.name}
+                  label="نام و نام خانوادگی"
+                  placeholder="علیرضا حسینی"
+                />
+                {fieldState.invalid && (
+                  <ErrorMessage message={fieldState.error?.message} />
+                )}
+              </div>
+            )}
+          />
+          <Controller
+            control={control}
+            name="phoneNumber"
+            render={({ field, fieldState }) => (
+              <div>
+                <NormalTextInput
+                  {...field}
+                  id={field.name}
+                  dir="ltr"
+                  label="شماره تماس"
+                  placeholder="09129876543"
+                />
+                {fieldState.invalid && (
+                  <ErrorMessage message={fieldState.error?.message} />
+                )}
+              </div>
+            )}
+          />
+          <Controller
+            control={control}
+            name="province"
+            render={({ field, fieldState }) => (
+              <div>
+                <NormalTextInput
+                  {...field}
+                  id={field.name}
+                  label="استان"
+                  placeholder="تهران"
+                />
+                {fieldState.invalid && (
+                  <ErrorMessage message={fieldState.error?.message} />
+                )}
+              </div>
+            )}
+          />
+          <Controller
+            control={control}
+            name="city"
+            render={({ field, fieldState }) => (
+              <div>
+                <NormalTextInput
+                  {...field}
+                  id={field.name}
+                  label="شهر"
+                  placeholder="تهران"
+                />
+                {fieldState.invalid && (
+                  <ErrorMessage message={fieldState.error?.message} />
+                )}
+              </div>
+            )}
+          />
+          <Controller
+            control={control}
+            name="additionalAddress"
+            render={({ field, fieldState }) => (
+              <div className="">
+                <NormalTextarea
+                  {...field}
+                  id={field.name}
+                  label="آدرس"
+                  placeholder="خیابان سعیدی-خیابان شکری-کوچه ۹-پلاک ۱۲"
+                />
+                {fieldState.invalid && (
+                  <ErrorMessage message={fieldState.error?.message} />
+                )}
+              </div>
+            )}
+          />
+          <Controller
+            control={control}
+            name="postCode"
+            render={({ field, fieldState }) => (
+              <div>
+                <NormalTextInput
+                  {...field}
+                  id={field.name}
+                  label="کد پستی"
+                  dir="ltr"
+                  placeholder="1897675421"
+                />
+                {fieldState.invalid && (
+                  <ErrorMessage message={fieldState.error?.message} />
+                )}
+              </div>
+            )}
+          />
+        </FieldGroup>
       </form>
     </section>
   );
